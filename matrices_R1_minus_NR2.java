de23b515265b543c05cbb721b3615d3dd0e82ee3
Jahanzaib Jaffer Ali <jahanzaib.jaffer111@gmail.com>
2026-02-26 09:56:47 +0500

    Fix input prompts and improve output messages

--- matrices_R1_minus_NR2.java
+++ matrices_R1_minus_NR2.java
@@ -5,16 +5,16 @@ public class matrices_R1_minus_NR2
     {
         try (Scanner read = new Scanner(System.in)) 
         {
-            System.out.println("Entre row of matrix");
+            System.out.println("Enter row of matrix");
             int row = read.nextInt();
             System.out.println();
-            System.out.println("Entre column of matrix");
+            System.out.println("Enter column of matrix");
             int column = read.nextInt();
             read.nextLine();
             System.out.println();
             int matrix1[][] = new int[row][column];
             int matrix2[][] = new int[row][column];
-            System.out.println("Entre matrix");
+            System.out.println("Enter matrix");
             for (int i = 0; i < row; i++) 
             {
                 for (int j = 0; j < column; j++) 
@@ -40,15 +40,15 @@ public class matrices_R1_minus_NR2
                     matrix2[i][j] = matrix1[i][j];
                 }
             }
-            System.out.println("Entre first row from which u want to minus");
+            System.out.println("Enter first row from which you want to minus");
             int row1=read.nextInt();
             row1-=1;
             System.out.println();
-            System.out.println("Entre second row from which u want to minus");
+            System.out.println("Enter second row to which uou want to minus");
             int row2=read.nextInt();
             row2-=1;
             System.out.println();
-            System.out.println("entre no u want to multiply with row of second matrix");
+            System.out.println("entre no which you want to multiply with second row you choose before");
             int no=read.nextInt();
             if(row1 >= 0 && row1 < row && row2 >= 0 && row2 < row)
             {
@@ -73,8 +73,9 @@ public class matrices_R1_minus_NR2
             }
             else
             {
-                System.out.println("maximum row is "+row);
+                System.out.println("maximum row present are "+row);
             }
        }
     }
+
 }
